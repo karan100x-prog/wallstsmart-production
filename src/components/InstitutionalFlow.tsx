6c9ac97dcebe3f3ef5dd538d7ff0bb547aaafe79
karan100x-prog <karan100x@gmail.com>
2025-09-06 16:14:00 +1000

    Update InstitutionalFlow.tsx

--- src/components/InstitutionalFlow.tsx
+++ src/components/InstitutionalFlow.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const InstitutionalFlow = () => {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Institutional Flow</h1>
-      <p className="text-gray-400">Coming soon...</p>
-    </div>
-  );
-};
-
-export default InstitutionalFlow;
