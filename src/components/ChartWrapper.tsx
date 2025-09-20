1078664534b0f41c2a1008ab3b960c7ec471c7bb
karan100x-prog <karan100x@gmail.com>
2025-09-21 07:42:51 +1000

    Update ChartWrapper.tsx

--- src/components/ChartWrapper.tsx
+++ src/components/ChartWrapper.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+interface ChartWrapperProps {
+  children: React.ReactNode;
+  height?: string;
+}
+
+const ChartWrapper: React.FC<ChartWrapperProps> = ({ children, height = '400px' }) => {
+  return (
+    <div className="w-full overflow-hidden rounded-lg bg-gray-900/50 p-4">
+      <div className="w-full" style={{ height, minHeight: height }}>
+        <div className="w-full h-full">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartWrapper;
