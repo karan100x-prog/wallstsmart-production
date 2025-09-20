023eac0383241a28a929a4e29c11249e83479bb7
karan100x-prog <karan100x@gmail.com>
2025-09-21 07:34:56 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,12 @@ export default {
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in-out',
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      }
     }
   },
   plugins: [],
