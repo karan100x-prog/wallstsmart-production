cd04182e8bc47047ea8ea4f99a3946f41001b6ce
karan100x-prog <karan100x@gmail.com>
2025-09-21 07:22:22 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,29 @@ export default {
         'dark-bg': '#0f1117',
         'dark-card': '#1a1d29',
         'green-primary': '#10b981'
-      }
+      },
+      screens: {
+        'xs': '475px',
+        '3xl': '1920px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+      maxWidth: {
+        '8xl': '88rem',
+        '9xl': '96rem',
+        'full': '100%',
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+      },
     }
   },
   plugins: [],
