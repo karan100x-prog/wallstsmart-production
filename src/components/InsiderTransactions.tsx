edf80e3360889b26e99ee770cf62d0c9139e9209
karan100x-prog <karan100x@gmail.com>
2025-09-24 22:31:30 +1000

    Update InsiderTransactions.tsx

--- src/components/InsiderTransactions.tsx
+++ src/components/InsiderTransactions.tsx
@@ -22,17 +22,6 @@ interface InsiderTransactionsProps {
 }
 
 
-import { apiDebugger } from '../services/apiDebugger';
-
-const fetchInsiderTransactions = async () => {
-  // ADD THIS LINE
-  apiDebugger.logCall(url, 'InsiderTransactions');
-  
-  // rest of your existing code...
-  const response = await fetch(url);
-  // ...
-};
-
 
 const InsiderTransactions: React.FC<InsiderTransactionsProps> = ({ symbol }) => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
